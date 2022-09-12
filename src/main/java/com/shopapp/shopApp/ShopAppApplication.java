9457264d33b3325461e34e6efd08ae9633963100
Filip7243 <filip7243@gmail.com>
2022-09-12 13:55:57 +0200

    init app

---
+++ src/main/java/com/shopapp/shopApp/ShopAppApplication.java
@@ -0,0 +1,13 @@
+package com.shopapp.shopApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopapp/shopApp/ShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopapp.shopApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
