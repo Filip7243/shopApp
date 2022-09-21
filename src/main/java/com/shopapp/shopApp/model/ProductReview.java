be55dcd0569bd5b63912dc8cb64ffe412e06861d
Filip7243 <filip7243@gmail.com>
2022-09-21 11:18:17 +0200

    added product-review moel

---
+++ src/main/java/com/shopapp/shopApp/model/ProductReview.java
@@ -0,0 +1,29 @@
+package com.shopapp.shopApp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductReview {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String topic;
+    private String description;
+    private Integer stars; // from 1 to 5
+    @ManyToOne
+    private Product product;
+    @ManyToOne
+    private AppUser user;
+}
