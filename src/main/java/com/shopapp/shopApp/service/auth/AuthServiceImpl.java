8f3be0a117577e881cd48b71ac4e2b1301ade2a1
Filip <filip7243@gmail.com>
2022-10-20 18:17:40 +0200

    in progress: failed signIn test, done: success signIn test

--- src/main/java/com/shopapp/shopApp/service/auth/AuthServiceImpl.java
+++ src/main/java/com/shopapp/shopApp/service/auth/AuthServiceImpl.java
@@ -18,11 +18,11 @@ import com.shopapp.shopApp.security.jwt.JwtUtils;
 import com.shopapp.shopApp.service.appuser.AppUserServiceImpl;
 import com.shopapp.shopApp.service.confirmationtoken.ConfirmationTokenServiceImpl;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -50,13 +50,11 @@ public class AuthServiceImpl implements AuthService {
     private final AppUserServiceImpl userService;
 
     @Override
-    public JwtResponse signInUser(LoginRequest loginRequest) {
+    public JwtResponse signInUser(LoginRequest loginRequest) throws AuthenticationException {
         Authentication authentication = authenticationManager
                 .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        System.out.println("ESSA");
 
         String accessToken = jwtUtils.generateJwtAccessToken(authentication);
         String refreshToken = jwtUtils.generateJwtRefreshToken(authentication);
@@ -131,11 +129,11 @@ public class AuthServiceImpl implements AuthService {
         PasswordResetToken foundToken = passwordResetTokenRepository.findByToken(token)
                 .orElseThrow(() -> new ConfirmationTokenNotFoundException(TOKEN_NOT_FOUND));
         // checks if token used
-        if(foundToken.getIsPasswordReset()) {
+        if (foundToken.getIsPasswordReset()) {
             throw new PasswordResetTokenException("Token already used");
         }
         // checks if token expired
-        if(foundToken.getExpiresAt().isBefore(LocalDateTime.now())) {
+        if (foundToken.getExpiresAt().isBefore(LocalDateTime.now())) {
             throw new PasswordResetTokenException("Token already expired!");
         }
         // setting new password from user input
@@ -149,70 +147,70 @@ public class AuthServiceImpl implements AuthService {
 
     private String buildEmail(String name, String link) {
         return "<div style=\"font-family:Helvetica,Arial,sans-serif;font-size:16px;margin:0;color:#0b0c0c\">\n" +
-               "\n" +
-               "<span style=\"display:none;font-size:1px;color:#fff;max-height:0\"></span>\n" +
-               "\n" +
-               "  <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;min-width:100%;width:100%!important\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\">\n" +
-               "    <tbody><tr>\n" +
-               "      <td width=\"100%\" height=\"53\" bgcolor=\"#0b0c0c\">\n" +
-               "        \n" +
-               "        <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;max-width:580px\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" align=\"center\">\n" +
-               "          <tbody><tr>\n" +
-               "            <td width=\"70\" bgcolor=\"#0b0c0c\" valign=\"middle\">\n" +
-               "                <table role=\"presentation\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
-               "                  <tbody><tr>\n" +
-               "                    <td style=\"padding-left:10px\">\n" +
-               "                  \n" +
-               "                    </td>\n" +
-               "                    <td style=\"font-size:28px;line-height:1.315789474;Margin-top:4px;padding-left:10px\">\n" +
-               "                      <span style=\"font-family:Helvetica,Arial,sans-serif;font-weight:700;color:#ffffff;text-decoration:none;vertical-align:top;display:inline-block\">Confirm your email</span>\n" +
-               "                    </td>\n" +
-               "                  </tr>\n" +
-               "                </tbody></table>\n" +
-               "              </a>\n" +
-               "            </td>\n" +
-               "          </tr>\n" +
-               "        </tbody></table>\n" +
-               "        \n" +
-               "      </td>\n" +
-               "    </tr>\n" +
-               "  </tbody></table>\n" +
-               "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
-               "    <tbody><tr>\n" +
-               "      <td width=\"10\" height=\"10\" valign=\"middle\"></td>\n" +
-               "      <td>\n" +
-               "        \n" +
-               "                <table role=\"presentation\" width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
-               "                  <tbody><tr>\n" +
-               "                    <td bgcolor=\"#1D70B8\" width=\"100%\" height=\"10\"></td>\n" +
-               "                  </tr>\n" +
-               "                </tbody></table>\n" +
-               "        \n" +
-               "      </td>\n" +
-               "      <td width=\"10\" valign=\"middle\" height=\"10\"></td>\n" +
-               "    </tr>\n" +
-               "  </tbody></table>\n" +
-               "\n" +
-               "\n" +
-               "\n" +
-               "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
-               "    <tbody><tr>\n" +
-               "      <td height=\"30\"><br></td>\n" +
-               "    </tr>\n" +
-               "    <tr>\n" +
-               "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
-               "      <td style=\"font-family:Helvetica,Arial,sans-serif;font-size:19px;line-height:1.315789474;max-width:560px\">\n" +
-               "        \n" +
-               "            <p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">Hi " + name + ",</p><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> Thank you for registering. Please click on the below link to activate your account: </p><blockquote style=\"Margin:0 0 20px 0;border-left:10px solid #b1b4b6;padding:15px 0 0.1px 15px;font-size:19px;line-height:25px\"><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> <a href=\"" + link + "\">Activate Now</a> </p></blockquote>\n Link will expire in 15 minutes. <p>See you soon</p>" +
-               "        \n" +
-               "      </td>\n" +
-               "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
-               "    </tr>\n" +
-               "    <tr>\n" +
-               "      <td height=\"30\"><br></td>\n" +
-               "    </tr>\n" +
-               "  </tbody></table><div class=\"yj6qo\"></div><div class=\"adL\">\n" +
-               "\n" +
-               "</div></div>";
+                "\n" +
+                "<span style=\"display:none;font-size:1px;color:#fff;max-height:0\"></span>\n" +
+                "\n" +
+                "  <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;min-width:100%;width:100%!important\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\">\n" +
+                "    <tbody><tr>\n" +
+                "      <td width=\"100%\" height=\"53\" bgcolor=\"#0b0c0c\">\n" +
+                "        \n" +
+                "        <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;max-width:580px\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" align=\"center\">\n" +
+                "          <tbody><tr>\n" +
+                "            <td width=\"70\" bgcolor=\"#0b0c0c\" valign=\"middle\">\n" +
+                "                <table role=\"presentation\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
+                "                  <tbody><tr>\n" +
+                "                    <td style=\"padding-left:10px\">\n" +
+                "                  \n" +
+                "                    </td>\n" +
+                "                    <td style=\"font-size:28px;line-height:1.315789474;Margin-top:4px;padding-left:10px\">\n" +
+                "                      <span style=\"font-family:Helvetica,Arial,sans-serif;font-weight:700;color:#ffffff;text-decoration:none;vertical-align:top;display:inline-block\">Confirm your email</span>\n" +
+                "                    </td>\n" +
+                "                  </tr>\n" +
+                "                </tbody></table>\n" +
+                "              </a>\n" +
+                "            </td>\n" +
+                "          </tr>\n" +
+                "        </tbody></table>\n" +
+                "        \n" +
+                "      </td>\n" +
+                "    </tr>\n" +
+                "  </tbody></table>\n" +
+                "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
+                "    <tbody><tr>\n" +
+                "      <td width=\"10\" height=\"10\" valign=\"middle\"></td>\n" +
+                "      <td>\n" +
+                "        \n" +
+                "                <table role=\"presentation\" width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
+                "                  <tbody><tr>\n" +
+                "                    <td bgcolor=\"#1D70B8\" width=\"100%\" height=\"10\"></td>\n" +
+                "                  </tr>\n" +
+                "                </tbody></table>\n" +
+                "        \n" +
+                "      </td>\n" +
+                "      <td width=\"10\" valign=\"middle\" height=\"10\"></td>\n" +
+                "    </tr>\n" +
+                "  </tbody></table>\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
+                "    <tbody><tr>\n" +
+                "      <td height=\"30\"><br></td>\n" +
+                "    </tr>\n" +
+                "    <tr>\n" +
+                "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
+                "      <td style=\"font-family:Helvetica,Arial,sans-serif;font-size:19px;line-height:1.315789474;max-width:560px\">\n" +
+                "        \n" +
+                "            <p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">Hi " + name + ",</p><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> Thank you for registering. Please click on the below link to activate your account: </p><blockquote style=\"Margin:0 0 20px 0;border-left:10px solid #b1b4b6;padding:15px 0 0.1px 15px;font-size:19px;line-height:25px\"><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> <a href=\"" + link + "\">Activate Now</a> </p></blockquote>\n Link will expire in 15 minutes. <p>See you soon</p>" +
+                "        \n" +
+                "      </td>\n" +
+                "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
+                "    </tr>\n" +
+                "    <tr>\n" +
+                "      <td height=\"30\"><br></td>\n" +
+                "    </tr>\n" +
+                "  </tbody></table><div class=\"yj6qo\"></div><div class=\"adL\">\n" +
+                "\n" +
+                "</div></div>";
     }
 }
--- src/test/java/com/shopapp/shopApp/service/AuthServiceTest.java
+++ src/test/java/com/shopapp/shopApp/service/AuthServiceTest.java
@@ -1,55 +1,113 @@
 package com.shopapp.shopApp.service;
 
 import com.shopapp.shopApp.dto.LoginRequest;
+import com.shopapp.shopApp.email.EmailSenderImpl;
 import com.shopapp.shopApp.model.AppUser;
+import com.shopapp.shopApp.model.AppUserRole;
 import com.shopapp.shopApp.repository.AppUserRepository;
+import com.shopapp.shopApp.repository.PasswordResetTokenRepository;
+import com.shopapp.shopApp.security.CustomPasswordEncoder;
 import com.shopapp.shopApp.security.jwt.JwtResponse;
 import com.shopapp.shopApp.security.jwt.JwtUtils;
+import com.shopapp.shopApp.service.appuser.AppUserServiceImpl;
 import com.shopapp.shopApp.service.auth.AuthServiceImpl;
+import com.shopapp.shopApp.service.confirmationtoken.ConfirmationTokenServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 
-import java.util.Optional;
+import java.util.HashSet;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthServiceTest {
 
-    @Mock
     private AppUserRepository userRepo;
-    @InjectMocks
-    private AuthServiceImpl authService;
-    @Mock
-    private Authentication auth;
-    @Mock
     private AuthenticationManager authenticationManager;
-    @Mock
     private JwtUtils jwtUtils;
+    private EmailSenderImpl emailSender;
+    private ConfirmationTokenServiceImpl tokenService;
+    private CustomPasswordEncoder passwordEncoder;
+    private PasswordResetTokenRepository passwordTokenRepo;
+    private AppUserServiceImpl userService;
+    private Authentication authentication;
+    private UsernamePasswordAuthenticationToken token;
+    private AuthServiceImpl authService;
+
+    @BeforeEach
+    void setUp() {
+        this.userRepo = Mockito.mock(AppUserRepository.class);
+        this.authenticationManager = Mockito.mock(AuthenticationManager.class);
+        this.jwtUtils = Mockito.mock(JwtUtils.class);
+        this.emailSender = Mockito.mock(EmailSenderImpl.class);
+        this.tokenService = Mockito.mock(ConfirmationTokenServiceImpl.class);
+        this.passwordEncoder = Mockito.mock(CustomPasswordEncoder.class);
+        this.passwordTokenRepo = Mockito.mock(PasswordResetTokenRepository.class);
+        this.userService = Mockito.mock(AppUserServiceImpl.class);
+        this.authentication = Mockito.mock(Authentication.class);
+        this.token = Mockito.mock(UsernamePasswordAuthenticationToken.class);
+
+        this.authService = new AuthServiceImpl(userRepo, authenticationManager, jwtUtils, emailSender, tokenService, passwordEncoder, passwordTokenRepo, userService);
+    }
 
     @Test
     void canSignInUser() {//todo;
-        String anyString = anyString();
-        AppUser user = new AppUser();
-        // to check if user exists spring uses override method loadByUsername() and that method uses findByEmail() from repo
-        // so when authenticationManager.authenticate() in authService is called, it calls loadByUsername()
-        when(userRepo.findByEmail(anyString)).thenReturn(Optional.of(user));
+        var anyString = anyString();
 
-        this.auth = authenticationManager
-                .authenticate(new UsernamePasswordAuthenticationToken(anyString, anyString));
-        authService.signInUser(new LoginRequest());
+        var loginRequest = new LoginRequest(anyString, anyString);
+
+        var user = new AppUser();
+        user.setEmail(loginRequest.getEmail());
+        user.setRoles(new HashSet<>());
+        assert user.getRoles() != null;
+        user.getRoles().add(new AppUserRole(null, "ROLE_USER", "USER"));
+
+        authentication.setAuthenticated(true);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        when(authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword())))
+                .thenReturn(authentication);
+        when(authentication.getPrincipal()).thenReturn(user);
+        when(jwtUtils.generateJwtAccessToken(authentication)).thenReturn(anyString);
+        when(jwtUtils.generateJwtRefreshToken(authentication)).thenReturn(anyString);
+        when(jwtUtils.getUsernameFromJwtToken(anyString)).thenReturn(anyString);
+
+        JwtResponse response = authService.signInUser(loginRequest);
+
+        assertThat(response).isNotNull();
+        assertThat(response).isInstanceOf(JwtResponse.class);
+        assertThat(response.getUsername()).isEqualTo(loginRequest.getEmail());
+        assertEquals(anyString, response.getAccessToken());
+        assertEquals(anyString, response.getRefreshToken());
+        assertThat(response.getAuthorities()).isNotNull();
+        assertThat(response.getAuthorities().size()).isEqualTo(1);
+    }
+
+    @Test
+    void canNotSignInUserBecauseAuthenticationFailed() {
+        var anyString = anyString();
+        var loginRequest = new LoginRequest("anyString", "anyString");
+
+        when(authenticationManager.authenticate(token)).thenReturn(authentication);
+
+        authService.signInUser(loginRequest);
+//        var ex = assertThrows(BadCredentialsException.class, () -> authService.signInUser(loginRequest));
+
     }
 
 
